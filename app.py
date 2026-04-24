3162de217283cc86c70e994a2fc99f55bfc99a4e
contatoviainvicta-dot <contato.viainvicta@gmail.com>
2026-04-23 22:36:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy.stats import linregress
 
 
 st.set_page_config(page_title="Painel Epidemiológico", layout="wide")
