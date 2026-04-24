13978ad7d66798d21f59fdf7616d6ca623e8d2b6
contatoviainvicta-dot <contato.viainvicta@gmail.com>
2026-04-24 09:59:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Criar dados manualmente (mais seguro que CSV bagunçado)
 dados = {
     "Ano": [2015,2016,2017,2018,2019,2020,2021,2022,2023,2024],
     "Casos": [203,220,283,388,298,291,355,399,320,146]
@@ -9,11 +8,11 @@ dados = {
 
 df = pd.DataFrame(dados)
 
-# Gráfico
 plt.figure()
 plt.plot(df["Ano"], df["Casos"], marker='o')
-plt.title("Sífilis Congênita - Distrito Federal")
+plt.title("Sífilis Congênita - DF")
 plt.xlabel("Ano")
 plt.ylabel("Casos")
 plt.grid()
+
 plt.show()
